5645ba10de21a136dabffc989b5c02704e313c7b
Eshita95 <eshita.islam95@gmail.com>
2022-09-25 21:07:57 +0600

    project done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import Navbar from './Navbar';
+import React from "react";
+import Navbar from "./Navbar"
+import Search from "./Search"
+import Chats from "./Chats"
 
 const SideBar = () => {
-    return (
-        <div className='sidebar'>
-            <Navbar/>
-        </div>
-    );
+  return (
+    <div className="sidebar">
+      <Navbar />
+      <Search/>
+      <Chats/>
+    </div>
+  );
 };
-
 export default SideBar;
