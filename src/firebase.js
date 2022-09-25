1f28f2b1eb32b42dcd8d4079b9d11629b404d040
Eshita95 <eshita.islam95@gmail.com>
2022-09-25 16:12:10 +0600

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDF-DawWZQsl4IbN6ihKRfWhj9Qy2XavgU",
+  authDomain: "social-chat-f6039.firebaseapp.com",
+  projectId: "social-chat-f6039",
+  storageBucket: "social-chat-f6039.appspot.com",
+  messagingSenderId: "762995889202",
+  appId: "1:762995889202:web:32c1b89484ed72f233d82b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
